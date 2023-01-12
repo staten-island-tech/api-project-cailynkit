22cdef7859eec0fe7936227028ccb86d62c0a564
cailynkit <cailynkitchens8@gmail.com>
2023-01-12 14:01:39 -0500

    fetch

--- api/js/main.js
+++ api/js/main.js
@@ -1,3 +1,12 @@
 const URL = "https://www.thecolorapi.com/";
 
-async;
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
