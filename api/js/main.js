27d5c42c418290ec29c40e78d19204f7efe2442b
cailynkit <cailynkitchens8@gmail.com>
2023-01-17 14:02:06 -0500

    idk

--- api/js/main.js
+++ api/js/main.js
@@ -1,15 +1,26 @@
-const URL = "https://aztro.sameerkumar.website";
+// const URL = "https://aztro.sameerkumar.website";
 
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-getData(URL);
+// async function getData(URL) {
+//   try {
+//     const response = await fetch(URL);
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// getData(URL);
+
+// function callback(error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body);
+//   }
+// }
+
+var options = {
+  url: "https://aztro.sameerkumar.website/?sign=aries&day=today",
+  method: "POST",
+};
 
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
