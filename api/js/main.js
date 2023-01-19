38c2ce593bbc69410bf531b345d89d6971ba1a5b
cailynkit <cailynkitchens8@gmail.com>
2023-01-19 14:01:34 -0500

    food

--- api/js/main.js
+++ api/js/main.js
@@ -11,12 +11,37 @@ const options = {
 
 const DOMSelectors = {
   choice: document.getElementById("choice"),
+  enter: document.getElementById("enter"),
+  clear: document.getElementById("clear"),
+  div: document.getElementById("div"),
 };
 
-//let food = `${DOMSelectors.choice.value}`;
-let food = "pancake";
+DOMSelectors.enter.addEventListener("click", function () {
+  let food = DOMSelectors.choice.value;
+  fetch(url + food, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      json.hints.forEach((number) =>
+        DOMSelectors.div.insertAdjacentHTML(
+          "beforeend",
+          `<p class="innerHTML">${number.food.label}</p>`
+        )
+      );
+    })
+    .catch((err) => console.error("error:" + err));
+});
 
-fetch(url + food, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error("error:" + err));
+DOMSelectors.clear.addEventListener("click", function () {
+  let innerHTML = document.querySelectorAll(".innerHTML");
+  innerHTML.remove();
+});
+
+/* DOMSelectors.clear.addEventListener("click", function () {
+  DOMSelectors.song.value = "";
+  DOMSelectors.artist.value = "";
+  let innerHTML = document.getElementById("innerHTML");
+  innerHTML.remove();
+  let innerHTMLButton = document.getElementById("innerHTMLButton");
+  innerHTMLButton.remove();
+}); */
